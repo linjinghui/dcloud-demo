bfb7b70fb95e962aa826b378623618f71b4141e2
linjinghui <852530363@qq.com>
2018-10-27 09:06:20 +0800

    fixed

--- js/api.js
+++ js/api.js
@@ -22,21 +22,15 @@
 			plus.webview.getLaunchWebview().show('pop-in');
 		}
 	}
-	/**
-	 * 获取图形验证码
-	 **/
-	owner.vcode = function() {
-		return URL + '/kaptcha?t=' + new Date().getTime();
-	}
 	
 	/**
-	 * 用户登录
+	 * post url带参方式
 	 **/
-	owner.login = function(params, callback) {
+	owner.login = function(pms, callback) {
 		var url = URL + '/login' + urlEncode({
-			username: params.account,
-			password: params.pwd,
-			kaptcha: params.code
+			username: pms.account,
+			password: pms.pwd,
+			kaptcha: pms.code
 		});
 		var wait = plus.nativeUI.showWaiting(WAIT_TEXT);
 		$.ajax(url, {
@@ -62,21 +56,7 @@
 	};
 	
 	/**
-	 * 保存登录用户信息
-	 **/
-	owner.saveUser = function(data) {
-		app.lsgSaveData(KEY_USER, data);
-	};
-	
-	/**
-	 * 获取登录用户信息
-	 **/
-	owner.getUserInfo = function() {
-		return app.lsgGetData(KEY_USER);
-	};
-	
-	/**
-	 * 退出登录
+	 * get方式
 	 **/
 	owner.loginout = function() {
 		var url = URL + '/logout';
@@ -85,6 +65,7 @@
 			dataType: 'json',
 			type: 'get',
 			timeout: TIMEOUT,
+//			data: {},
 			headers: {'Content-Type':'application/json'},
 			error: function (xhr, type, errorThrown) {
 				wait.close();
@@ -99,215 +80,37 @@
 		// 清除缓存
 		app.lsgDeleteData();
 	};
-	
+
 	/**
-	 * 获取 主体基本信息
+	 * POST表单提交
 	 **/
-	owner.getEntInfo = function(callback) {
+	owner.formPost = function(pms, callback) {
 		var userInfo = owner.getUserInfo();
-		var url = URL + '/ent_sys_entuser/getEntUser';
-		var wait = plus.nativeUI.showWaiting(WAIT_TEXT);
-		$.ajax(url, {
-			dataType: 'json',
-			type: 'get',
-			timeout: TIMEOUT,
-			data:{
-				username: userInfo.username
-			},
-			error: function (xhr, type, errorThrown) {
-				wait.close();
-				plus.nativeUI.toast(type);
-			},
-			success: function (data) {
-				wait.close();
-				filterCode(data);
-				callback && callback(data);
-				if (data.code !== 0) {
-					plus.nativeUI.toast(data.msg);
-				} else {
-					// 保存主体信息
-					userInfo.entInfo = data.ret;
-					owner.saveUser(userInfo);
-				}
-			}
-		});
-	}
-	
-	/**
-	 * 获取 证照信息
-	 **/
-	owner.getZzInfo = function(callback) {
-		var userInfo = owner.getUserInfo();
-		console.log(JSON.stringify(userInfo));
-		var url = URL + '/ent_main_license/getAllInfo';
-		var wait = plus.nativeUI.showWaiting(WAIT_TEXT);
-		$.ajax(url, {
-			dataType: 'json',
-			type: 'get',
-			timeout: TIMEOUT,
-			data:{
-				entId: userInfo.entInfo.entId
-			},
-			error: function (xhr, type, errorThrown) {
-				wait.close();
-				plus.nativeUI.toast(type);
-			},
-			success: function (data) {
-				wait.close();
-				filterCode(data);
-				callback && callback(data);
-				if (data.code !== 0) {
-					plus.nativeUI.toast(data.msg);
-				}
-			}
-		});
-	}
-	
-	/**
-	 * 获取 认证信息列表
-	 **/
-	owner.getRzDataList = function(callback) {
-		var userInfo = owner.getUserInfo();
-		var url = URL + '/ent_main_certification/getAllInfo';
-		var wait = plus.nativeUI.showWaiting(WAIT_TEXT);
-		$.ajax(url, {
-			dataType: 'json',
-			type: 'get',
-			timeout: TIMEOUT,
-			data:{
-				entId: userInfo.entInfo.entId
-			},
-			error: function (xhr, type, errorThrown) {
-				wait.close();
-				plus.nativeUI.toast(type);
-			},
-			success: function (data) {
-				wait.close();
-				filterCode(data);
-				callback && callback(data);
-				if (data.code !== 0) {
-					plus.nativeUI.toast(data.msg);
-				}
-			}
-		});
-	}
-	
-	/**
-	 * 获取 生产基地信息列表
-	 **/
-	owner.getScjdDataList = function(callback) {
-		var userInfo = owner.getUserInfo();
-		var url = URL + '/ent_main_produce_place/getAllInfo';
-		var wait = plus.nativeUI.showWaiting(WAIT_TEXT);
-		$.ajax(url, {
-			dataType: 'json',
-			type: 'get',
-			timeout: TIMEOUT,
-			data:{
-				entId: userInfo.entInfo.entId
-			},
-			error: function (xhr, type, errorThrown) {
-				wait.close();
-				plus.nativeUI.toast(type);
-			},
-			success: function (data) {
-				wait.close();
-				filterCode(data);
-				callback && callback(data);
-				if (data.code !== 0) {
-					plus.nativeUI.toast(data.msg);
-				}
-			}
-		});
-	}
-	
-	/**
-	 * 获取 电商信息列表
-	 **/
-	owner.getDsDataList = function(callback) {
-		var userInfo = owner.getUserInfo();
-		var url = URL + '/ent_main_recommend/getAllInfo';
-		var wait = plus.nativeUI.showWaiting(WAIT_TEXT);
-		$.ajax(url, {
-			dataType: 'json',
-			type: 'get',
-			timeout: TIMEOUT,
-			data:{
-				entId: userInfo.entInfo.entId
-			},
-			error: function (xhr, type, errorThrown) {
-				wait.close();
-				plus.nativeUI.toast(type);
-			},
-			success: function (data) {
-				wait.close();
-				filterCode(data);
-				callback && callback(data);
-				if (data.code !== 0) {
-					plus.nativeUI.toast(data.msg);
-				}
-			}
-		});
-	}
-	
-	/**
-	 * 获取 主体形象信息
-	 **/
-	owner.getZtxxInfo = function(callback) {
-		var userInfo = owner.getUserInfo();
-		var url = URL + '/ent_main_sub_info/getInfo';
-		var wait = plus.nativeUI.showWaiting(WAIT_TEXT);
-		$.ajax(url, {
-			dataType: 'json',
-			type: 'get',
-			timeout: TIMEOUT,
-			data:{
-				entId: userInfo.entInfo.entId
-			},
-			error: function (xhr, type, errorThrown) {
-				wait.close();
-				plus.nativeUI.toast(type);
-			},
-			success: function (data) {
-				wait.close();
-				filterCode(data);
-				callback && callback(data);
-				if (data.code !== 0) {
-					plus.nativeUI.toast(data.msg);
-				}
-			}
-		});
-	}
-	
-	/**
-	 * 更新 主体形象信息
-	 **/
-	owner.getSaveZtxxInfo = function(params, callback) {
-		var userInfo = owner.getUserInfo();
-		var url = URL + '/ent_main_sub_info/saveOrUpdate';
-//		var url = URL + '/ent_main_sub_info/saveOrUpdate' + urlEncode({
-//			entId: userInfo.entInfo.entId,
-//			entDescription: params.entDescription,
-//			entWebsite: params.entWebsite
-//		});
+		var url = URL + '/ent_main_produce_place/saveOrUpdate';
 		var wait = plus.nativeUI.showWaiting(WAIT_TEXT);
 		$.ajax(url, {
 			dataType: 'json',
 			type: 'post',
 			timeout: TIMEOUT,
-//			headers: {'Content-Type':'application/json'},
 			data: {
+				pplaceId: pms.pplaceId || '',
 				entId: userInfo.entInfo.entId,
-				entDescription: params.entDescription,
-				entWebsite: params.entWebsite
+				placeName: pms.placeName,
+				placeDescription: pms.placeDescription,
+				placeLongitude: pms.placeLongitude,
+				placeLatitude: pms.placeLatitude,
+				placeArea: pms.placeArea,
+				contactName: pms.contactName,
+				contactNumber: pms.contactNumber,
+				placeCategory: pms._placeCategory
 			},
 			error: function (xhr, type, errorThrown) {
 				wait.close();
 				plus.nativeUI.toast(type);
 			},
 			success: function (data) {
-				console.log(JSON.stringify(data));
 				wait.close();
+				filterCode(data);
 				callback && callback(data);
 				if (data.code !== 0) {
 					plus.nativeUI.toast(data.msg);
@@ -315,4 +118,6 @@
 			}
 		});
 	}
+	
+	
 }(mui, window.api = {}));
